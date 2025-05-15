d65b360c4d30398f4f166a2c913ea643e51938b5
Jendrik0 <mail@jdrk.de>
2025-05-15 13:41:04 +0200

    Update subnet address prefixes and modify resource prefix variable

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "azurerm_subnet" "bastion" {
   name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.3.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 # > Subnet for Managed SQL Instance
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "subnet_db" {
   name                 = "${var.prefix}-subnet-db"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.3.0/24"]
 
   delegation {
     name = "managedinstancedelegation"
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "prefix" {
   description = "Prefix for all resources"
   type        = string
-  default     = "ap2"
+  default     = "ap"
 }
 variable "location" {
   description = "Azure location"
@@ -10,10 +10,6 @@ variable "location" {
 }
 variable "admin_username" {
   description = "Admin username for the VM"
-  type        = string
-}
-variable "admin_password" {
-  description = "Admin password for the VM"
   type        = string
 }
 variable "subscription_id" {
